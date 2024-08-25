68cc98975e3f048f6c78d69194088859261e4d9c
Shy-El <shaielshmoel@gmail.com>
2024-08-25 11:09:42 +0300

    comm3 v1

--- NPB-OMP-GPU/MG/mg.cpp
+++ NPB-OMP-GPU/MG/mg.cpp
@@ -481,27 +481,36 @@ static void comm3(void* pointer_u, int n1, int n2, int n3, int kk){
 
 	int i1, i2, i3;
 	if(timeron){timer_start(T_COMM3);}
-	/* axis = 1 */
-	for(i3 = 1; i3 < n3-1; i3++){
-		for(i2 = 1; i2 < n2-1; i2++){
-			u[i3][i2][0] = u[i3][i2][n1-2];
-			u[i3][i2][n1-1] = u[i3][i2][1];			
+	#pragma omp target enter data map(to: u[:n3][:n2][:n1])
+	{
+		/* axis = 1 */
+		#pragma omp target teams distribute parallel for collapse(2)
+		for(i3 = 1; i3 < n3-1; i3++){
+			for(i2 = 1; i2 < n2-1; i2++){
+				u[i3][i2][0] = u[i3][i2][n1-2];
+				u[i3][i2][n1-1] = u[i3][i2][1];			
+			}
+		}
+
+		/* axis = 2 */
+		#pragma omp target teams distribute parallel for collapse(2)
+		for(i3 = 1; i3 < n3-1; i3++){
+			for(i1 = 0; i1 < n1; i1++){
+				u[i3][0][i1] = u[i3][n2-2][i1];
+				u[i3][n2-1][i1] = u[i3][1][i1];			
+			}
+		}
+
+		/* axis = 3 */
+		#pragma omp target teams distribute parallel for collapse(2)
+		for(i2 = 0; i2 < n2; i2++){
+			for(i1 = 0; i1 < n1; i1++){
+				u[0][i2][i1] = u[n3-2][i2][i1];
+				u[n3-1][i2][i1] = u[1][i2][i1];			
+			}
 		}
 	}
-	/* axis = 2 */
-	for(i3 = 1; i3 < n3-1; i3++){
-		for(i1 = 0; i1 < n1; i1++){
-			u[i3][0][i1] = u[i3][n2-2][i1];
-			u[i3][n2-1][i1] = u[i3][1][i1];			
-		}
-	}
-	/* axis = 3 */
-	for(i2 = 0; i2 < n2; i2++){
-		for(i1 = 0; i1 < n1; i1++){
-			u[0][i2][i1] = u[n3-2][i2][i1];
-			u[n3-1][i2][i1] = u[1][i2][i1];			
-		}
-	}
+	#pragma omp target exit data map(from: u[:n3][:n2][:n1])
 	if(timeron){timer_stop(T_COMM3);}
 }
 
